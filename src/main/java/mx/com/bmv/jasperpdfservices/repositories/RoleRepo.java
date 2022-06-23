6bb6eeda4e7b4cd16b154eab50d33807e9f40544
Alexander Garcia <alex.garci@live.com>
2022-06-23 09:10:00 -0500

    Change Id to UUID in repos

--- src/main/java/mx/com/bmv/jasperpdfservices/repositories/RoleRepo.java
+++ src/main/java/mx/com/bmv/jasperpdfservices/repositories/RoleRepo.java
@@ -4,8 +4,10 @@ import mx.com.bmv.jasperpdfservices.models.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface RoleRepo extends JpaRepository<Role, Long> {
+public interface RoleRepo extends JpaRepository<Role, UUID> {
 
     Role findByDescription(String description);
 }
